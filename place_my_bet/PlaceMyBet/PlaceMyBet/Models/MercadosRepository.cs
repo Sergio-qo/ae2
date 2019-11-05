3ff44683cecbffa915b555956917e6fcc2f8e70a
Sergio-qo <sergio.qo000@gmail.com>
2019-11-05 16:45:15 +0100

    ae5 punto 1 hecho en mercadosrepository

--- place_my_bet/PlaceMyBet/PlaceMyBet/Models/MercadosRepository.cs
+++ place_my_bet/PlaceMyBet/PlaceMyBet/Models/MercadosRepository.cs
@@ -8,102 +8,103 @@ namespace PlaceMyBet.Models
 {
     public class MercadosRepository
     {
-        private MySqlConnection Connect()
+        /*private MySqlConnection Connect()
         {
             string connString = "Server=localhost;Port=3306;Database=mydb;Uid=root;password=;SslMode=none";
             MySqlConnection con = new MySqlConnection(connString);
             return con;
-        }
+        }*/
         internal List<Mercado> Retrieve()
         {
-            MySqlConnection con = Connect();
+            /*MySqlConnection con = Connect();
             MySqlCommand command = con.CreateCommand();
-            command.CommandText = "select * from mercado";
+            command.CommandText = "select * from mercado";*/
             try
             {
-                con.Open();
+                /*con.Open();
                 MySqlDataReader reader = command.ExecuteReader();
                 List<Mercado> mercados = new List<Mercado>();
                 while (reader.Read())
                 {
                     mercados.Add(new Mercado(reader.GetInt32(0), reader.GetDouble(1), reader.GetDouble(2), reader.GetDouble(3), reader.GetDouble(4), reader.GetString(5), reader.GetInt32(6)));
                 }
-                con.Close();
-                return mercados;
+                con.Close();*/
+                return null;
             }
-            catch (MySqlException ex)
+            catch (/*MySqlException ex*/InvalidCastException e)
             {
-                Console.WriteLine("Se ha producido un error: " + ex);
+                Console.WriteLine("Se ha producido un error: " + e);
                 return null;
             }
         }
 
         internal List<MercadoDTO> RetrieveDTO()
         {
-            MySqlConnection con = Connect();
+            /*MySqlConnection con = Connect();
             MySqlCommand command = con.CreateCommand();
-            command.CommandText = "select * from mercado";
+            command.CommandText = "select * from mercado";*/
             try
             {
-                con.Open();
+                /*con.Open();
                 MySqlDataReader reader = command.ExecuteReader();
                 List<MercadoDTO> mercados = new List<MercadoDTO>();
                 while (reader.Read())
                 {
                     mercados.Add(new MercadoDTO(reader.GetDouble(3), reader.GetDouble(4), reader.GetString(5), reader.GetInt32(6)));
                 }
-                con.Close();
-                return mercados;
+                con.Close();*/
+                return null;
             }
-            catch (MySqlException ex)
+            catch (/*MySqlException ex*/ InvalidCastException e)
             {
-                Console.WriteLine("Se ha producido un error: " + ex);
+                Console.WriteLine("Se ha producido un error: " + e);
                 return null;
             }
         }
 
         internal List<Mercado> RetrieveByEventoId(int id_evento)
         {
-            MySqlConnection con = Connect();
+            /*MySqlConnection con = Connect();
             MySqlCommand command = con.CreateCommand();
-            command.CommandText = "SELECT * FROM mercado WHERE id_partido = " + id_evento +";";
+            command.CommandText = "SELECT * FROM mercado WHERE id_partido = " + id_evento +";";*/
             try
             {
-                con.Open();
+                /*con.Open();
                 MySqlDataReader reader = command.ExecuteReader();
                 List<Mercado> mercados = new List<Mercado>();
                 while (reader.Read())
                 {
                     mercados.Add(new Mercado(reader.GetInt32(0), reader.GetDouble(1), reader.GetDouble(2), reader.GetDouble(3), reader.GetDouble(4), reader.GetString(5), reader.GetInt32(6)));
-                }
-                return mercados;
+                }*/
+                return null;
             }
-            catch (MySqlException ex)
+            catch (/*MySqlException ex*/ InvalidCastException e)
             {
-                Console.WriteLine(ex);
+                Console.WriteLine(e);
                 return null;
             }
         }
 
         internal List<MercadoDTOAP> RetrieveAP(int id_mercado)
         {
-            MySqlConnection con = Connect();
+            /*MySqlConnection con = Connect();
             MySqlCommand command = con.CreateCommand();
             command.CommandText = "SELECT a.email_usuario, m.tipo, a.tipo, a.dinero_apostado, a.cuota FROM mercado m, apuesta a WHERE a.id_mercado = m.id and m.id = " + id_mercado+";";
+            */
             try
             {
-                con.Open();
+                /*con.Open();
                 MySqlDataReader reader = command.ExecuteReader();
                 List<MercadoDTOAP> mercados = new List<MercadoDTOAP>();
                 while (reader.Read())
                 {
                     mercados.Add(new MercadoDTOAP(reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetDouble(4), reader.GetDouble(3)));
-                }
-                return mercados;
+                }*/
+                return null;
             }
-            catch (MySqlException ex)
+            catch (/*MySqlException ex*/ InvalidCastException e)
             {
-                Console.WriteLine(ex);
+                Console.WriteLine(e);
                 return null;
             }
         }
