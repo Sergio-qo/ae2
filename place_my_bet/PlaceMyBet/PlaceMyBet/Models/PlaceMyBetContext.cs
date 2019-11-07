1361cc0e98ab7a2fe1c5bc493563b5bbc1413905
Sergio-qo <sergio.qo000@gmail.com>
2019-11-07 15:08:49 +0100

    punto 3 ae5 hecho

---
+++ place_my_bet/PlaceMyBet/PlaceMyBet/Models/PlaceMyBetContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.EntityFrameworkCore;
+
+namespace PlaceMyBet.Models
+{
+    public class PlaceMyBetContext : DbContext
+    {
+        public DbSet<Mercado> Mercados { get; set; }
+        public DbSet<Evento> Eventos{ get; set; }
+        public DbSet<Apuesta> Apuestas{ get; set; }
+        public DbSet<Cuenta> Cuentas{ get; set; }
+        public DbSet<Usuario> Usuarios{ get; set; }
+
+        public PlaceMyBetContext()
+        {
+
+        }
+
+        public PlaceMyBetContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql("Server=localhost;Database=placemybet;Uid=root;Pwd='';SslMode=none");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
